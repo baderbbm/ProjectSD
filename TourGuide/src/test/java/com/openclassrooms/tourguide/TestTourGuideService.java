13db5b5b5686412bf7c6586e222b2fd8095675c5
baderbbm <131297523+baderbbm@users.noreply.github.com>
2023-11-29 20:58:36 +0100

    Update TestTourGuideService.java

--- TourGuide/src/test/java/com/openclassrooms/tourguide/TestTourGuideService.java
+++ TourGuide/src/test/java/com/openclassrooms/tourguide/TestTourGuideService.java
@@ -3,6 +3,7 @@ package com.openclassrooms.tourguide;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 
@@ -11,13 +12,20 @@ import org.junit.jupiter.api.Test;
 
 import gpsUtil.GpsUtil;
 import gpsUtil.location.Attraction;
+import gpsUtil.location.Location;
 import gpsUtil.location.VisitedLocation;
 import rewardCentral.RewardCentral;
+
+import com.openclassrooms.tourguide.dto.AttractionInfo;
 import com.openclassrooms.tourguide.helper.InternalTestHelper;
 import com.openclassrooms.tourguide.service.RewardsService;
 import com.openclassrooms.tourguide.service.TourGuideService;
 import com.openclassrooms.tourguide.user.User;
 import tripPricer.Provider;
+import com.openclassrooms.tourguide.user.UserReward;
+
+import gpsUtil.location.Attraction;
+import gpsUtil.location.VisitedLocation;
 
 public class TestTourGuideService {
 
@@ -92,7 +100,12 @@ public class TestTourGuideService {
 		assertEquals(user.getUserId(), visitedLocation.userId);
 	}
 
-	@Disabled // Not yet implemented
+	// @Disabled // Not yet implemented
+
+	// les 5 attractions les plus proches de la dernière localisation de
+	// l'utilisateur
+
+	/*
 	@Test
 	public void getNearbyAttractions() {
 		GpsUtil gpsUtil = new GpsUtil();
@@ -109,20 +122,50 @@ public class TestTourGuideService {
 
 		assertEquals(5, attractions.size());
 	}
-
-	public void getTripDeals() {
+	*/
+	
+	@Test
+	public void getNearbyAttractions() {
 		GpsUtil gpsUtil = new GpsUtil();
 		RewardsService rewardsService = new RewardsService(gpsUtil, new RewardCentral());
 		InternalTestHelper.setInternalUserNumber(0);
 		TourGuideService tourGuideService = new TourGuideService(gpsUtil, rewardsService);
 
 		User user = new User(UUID.randomUUID(), "jon", "000", "jon@tourGuide.com");
+		VisitedLocation visitedLocation = tourGuideService.trackUserLocation(user);
+		
+		List<AttractionInfo> attractions = tourGuideService.getNearByAttractions(visitedLocation, user);
+
+		tourGuideService.tracker.stopTracking();
+
+		assertEquals(5, attractions.size());
+	}
+	
+
+	@Test
+	public void getTripDeals() {
+		GpsUtil gpsUtil = new GpsUtil();
+		RewardsService rewardsService = new RewardsService(gpsUtil, new RewardCentral());
+		InternalTestHelper.setInternalUserNumber(0);
+		TourGuideService tourGuideService = new TourGuideService(gpsUtil, rewardsService);
+		UUID userId = UUID.randomUUID();
+
+		// User user = new User(UUID.randomUUID(), "jon", "000", "jon@tourGuide.com");
+		User user = new User(userId, "jon", "000", "jon@tourGuide.com");
+
+		// Attraction(java.lang.String attractionName, java.lang.String city,
+		// java.lang.String state, double latitude, double longitude)
+		Attraction attraction = new Attraction("bb", "Paris", "France", 2000, 1000);
+		// VisitedLocation(java.util.UUID userId, gpsUtil.location.Location location,
+		// java.util.Date timeVisited)
+		VisitedLocation visitedLocation = new VisitedLocation(userId, attraction, new Date());
+		user.addUserReward(new UserReward(visitedLocation, attraction, 2000000000));
 
 		List<Provider> providers = tourGuideService.getTripDeals(user);
 
 		tourGuideService.tracker.stopTracking();
 
-		assertEquals(10, providers.size());
+		assertEquals(5, providers.size());
 	}
 
 }
