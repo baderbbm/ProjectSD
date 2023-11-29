d24072f0496116d889ee1a49f99a531d3323144d
baderbbm <131297523+baderbbm@users.noreply.github.com>
2023-11-29 20:46:12 +0100

    Update TourGuideController.java

--- TourGuide/src/main/java/com/openclassrooms/tourguide/TourGuideController.java
+++ TourGuide/src/main/java/com/openclassrooms/tourguide/TourGuideController.java
@@ -1,19 +1,17 @@
 package com.openclassrooms.tourguide;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 import gpsUtil.location.Attraction;
 import gpsUtil.location.VisitedLocation;
 
+import com.openclassrooms.tourguide.dto.AttractionInfo;
 import com.openclassrooms.tourguide.service.TourGuideService;
 import com.openclassrooms.tourguide.user.User;
 import com.openclassrooms.tourguide.user.UserReward;
-
 import tripPricer.Provider;
 
 @RestController
@@ -41,10 +39,19 @@ public class TourGuideController {
         // The distance in miles between the user's location and each of the attractions.
         // The reward points for visiting each Attraction.
         //    Note: Attraction reward points can be gathered from RewardsCentral
+   
+    /*
     @RequestMapping("/getNearbyAttractions") 
     public List<Attraction> getNearbyAttractions(@RequestParam String userName) {
     	VisitedLocation visitedLocation = tourGuideService.getUserLocation(getUser(userName));
     	return tourGuideService.getNearByAttractions(visitedLocation);
+    }
+    */
+    
+    @RequestMapping("/getNearbyAttractions") 
+    public List<AttractionInfo> getNearbyAttractions(@RequestParam String userName, @RequestParam User user) {
+    	VisitedLocation visitedLocation = tourGuideService.getUserLocation(getUser(userName));
+    	return tourGuideService.getNearByAttractions(visitedLocation, user);
     }
     
     @RequestMapping("/getRewards") 
@@ -60,6 +67,4 @@ public class TourGuideController {
     private User getUser(String userName) {
     	return tourGuideService.getUser(userName);
     }
-   
-
 }
