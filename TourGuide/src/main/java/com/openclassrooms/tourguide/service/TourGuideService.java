6c440a784f5bb3605da09cfb3c0b711d9812dcfc
baderbbm <131297523+baderbbm@users.noreply.github.com>
2023-11-29 20:56:36 +0100

    Update TourGuideService.java

--- TourGuide/src/main/java/com/openclassrooms/tourguide/service/TourGuideService.java
+++ TourGuide/src/main/java/com/openclassrooms/tourguide/service/TourGuideService.java
@@ -1,13 +1,14 @@
 package com.openclassrooms.tourguide.service;
 
+import com.openclassrooms.tourguide.dto.AttractionInfo;
 import com.openclassrooms.tourguide.helper.InternalTestHelper;
 import com.openclassrooms.tourguide.tracker.Tracker;
 import com.openclassrooms.tourguide.user.User;
 import com.openclassrooms.tourguide.user.UserReward;
-
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
@@ -17,18 +18,17 @@ import java.util.Random;
 import java.util.UUID;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
-
 import gpsUtil.GpsUtil;
 import gpsUtil.location.Attraction;
 import gpsUtil.location.Location;
 import gpsUtil.location.VisitedLocation;
-
 import tripPricer.Provider;
 import tripPricer.TripPricer;
+import rewardCentral.RewardCentral;
+
 
 @Service
 public class TourGuideService {
@@ -79,25 +79,70 @@ public class TourGuideService {
 		}
 	}
 
+	/*effectue des calculs basés sur les points de récompense de 
+	l'utilisateur, puis appelle un service externe (tripPricer) pour obtenir 
+	des offres de voyage en fonction des préférences de l'utilisateur
+	Les offres sont ensuite associées à l'utilisateur et renvoyées */
+	
+	
+	
 	public List<Provider> getTripDeals(User user) {
 		int cumulatativeRewardPoints = user.getUserRewards().stream().mapToInt(i -> i.getRewardPoints()).sum();
+		System.out.println("Cumulative Reward Points: " + cumulatativeRewardPoints);
+//  getPrice(java.lang.String apiKey, java.util.UUID attractionId, int adults, int children, int nightsStay, int rewardsPoints)
 		List<Provider> providers = tripPricer.getPrice(tripPricerApiKey, user.getUserId(),
 				user.getUserPreferences().getNumberOfAdults(), user.getUserPreferences().getNumberOfChildren(),
 				user.getUserPreferences().getTripDuration(), cumulatativeRewardPoints);
 		user.setTripDeals(providers);
+		System.out.println("Providers from tripPricer: " + providers);
 		return providers;
 	}
+	
+	
+	
+	
+	
+	/*
+	public List<Provider> getTripDeals(User user) {
+	    int cumulativeRewardPoints = user.getUserRewards().stream().mapToInt(i -> i.getRewardPoints()).sum();
+	    System.out.println("Cumulative Reward Points: " + cumulativeRewardPoints);
+	    List<Provider> providersWithRewardPoints = tripPricer.getPrice(
+	        tripPricerApiKey, user.getUserId(),
+	        user.getUserPreferences().getNumberOfAdults(), user.getUserPreferences().getNumberOfChildren(),
+	        user.getUserPreferences().getTripDuration(), cumulativeRewardPoints
+	    );
 
+	    // Get Additional 5 Trip Deals without Considering Reward Points
+	    List<Provider> additionalProviders = tripPricer.getPrice(
+	        tripPricerApiKey, user.getUserId(),
+	        user.getUserPreferences().getNumberOfAdults(), user.getUserPreferences().getNumberOfChildren(),
+	        user.getUserPreferences().getTripDuration(), 0 // No cumulative reward points considered
+	    );
+
+	    // Combine the two lists of providers
+	    List<Provider> allProviders = new ArrayList<>(providersWithRewardPoints);
+	    allProviders.addAll(additionalProviders);
+
+	    // Set Trip Deals for the User
+	    user.setTripDeals(allProviders);
+
+	    // Print Providers to Console
+	  System.out.println("Providers from tripPricer: " + allProviders);
+	    return allProviders;
+	}
+
+*/
 	public VisitedLocation trackUserLocation(User user) {
 		VisitedLocation visitedLocation = gpsUtil.getUserLocation(user.getUserId());
 		user.addToVisitedLocations(visitedLocation);
 		rewardsService.calculateRewards(user);
 		return visitedLocation;
 	}
-
+/*
 	public List<Attraction> getNearByAttractions(VisitedLocation visitedLocation) {
 		List<Attraction> nearbyAttractions = new ArrayList<>();
 		for (Attraction attraction : gpsUtil.getAttractions()) {
+		        // Vérifie si l'attraction est dans la proximité de la localisation visitée par l'utilisateur
 			if (rewardsService.isWithinAttractionProximity(attraction, visitedLocation.location)) {
 				nearbyAttractions.add(attraction);
 			}
@@ -105,6 +150,97 @@ public class TourGuideService {
 
 		return nearbyAttractions;
 	}
+	*/
+
+	public List<AttractionInfo> getNearByAttractions(VisitedLocation visitedLocation, User user) {
+	    List<Attraction> allAttractions = gpsUtil.getAttractions();
+
+	    // Trier la liste allAttractions en fonction de la distance entre chaque attraction et la dernière localisation de l'utilisateur
+	    allAttractions.sort(
+	            Comparator.comparingDouble(attraction -> rewardsService.getDistance(attraction, visitedLocation.location)));
+
+	    // Obtenir les 5 attractions les plus proches
+	    List<Attraction> nearbyAttractions = allAttractions.stream().limit(5).collect(Collectors.toList());
+
+	    // Créer une liste d'objets AttractionInfo pour contenir les informations demandées
+	    List<AttractionInfo> result = new ArrayList<>();
+
+	    // Remplir la liste avec les informations de chaque attraction
+	    for (Attraction attraction : nearbyAttractions) {
+	        double distance = rewardsService.getDistance(attraction, visitedLocation.location);
+	      int rewardPoints = rewardsService.getRewardPoints(attraction, user);
+
+	        AttractionInfo attractionInfo = new AttractionInfo(
+	                attraction.attractionName,
+	                attraction.latitude,
+	                attraction.longitude,
+	                visitedLocation.location.latitude,
+	                visitedLocation.location.longitude,
+	                distance,
+	                rewardPoints
+	        );
+
+	        result.add(attractionInfo);
+	    }
+
+	    return result;
+	}
+
+	
+public List<Attraction> getNearByAttractions(VisitedLocation visitedLocation) {
+	List<Attraction> allAttractions = gpsUtil.getAttractions();
+
+	// Trier la liste allAttractions en fonction de la distance entre chaque attraction et la dernière localisation de l'utilisateur
+
+	allAttractions.sort(
+			Comparator.comparingDouble(attraction -> rewardsService.getDistance(attraction, visitedLocation.location)));
+
+	// Obtenir les 5 attractions les plus proches
+
+	List<Attraction> nearbyAttractions = allAttractions.stream().limit(5).collect(Collectors.toList());
+
+	return nearbyAttractions;
+}
+
+
+/*
+public List<Attraction> getNearByAttractions(VisitedLocation visitedLocation) {
+    List<Attraction> nearbyAttractions = gpsUtil.getAttractions().stream()
+            .filter(attraction -> rewardsService.isWithinAttractionProximity(attraction, visitedLocation.location))
+        	// limit(5): limite le flux aux 5 premiers éléments qui satisfont la condition du filtre 
+            // le traitement s'arrêtera dès qu'il aura trouvé les 5 premières attractions à proximité
+            .limit(5)
+            .collect(Collectors.toList());
+
+    System.out.println("Nombre d'attractions à proximité : " + nearbyAttractions.size());
+
+    return nearbyAttractions;
+}
+
+public List<Attraction> getNearByAttractions(VisitedLocation visitedLocation) {
+    List<Attraction> nearbyAttractions = gpsUtil.getAttractions().stream()
+            .filter(attraction -> rewardsService.isWithinAttractionProximity(attraction, visitedLocation.location))
+            .limit(5)
+            .collect(Collectors.toList());
+
+    // Si le nombre d'attractions à proximité est inférieur à 5, compléter avec d'autres attractions non filtrées
+    
+    if (nearbyAttractions.size() < 5) {
+        List<Attraction> additionalAttractions = gpsUtil.getAttractions().stream()
+                .filter(attraction -> !nearbyAttractions.contains(attraction)) // Éviter les duplicatas
+                .limit(5 - nearbyAttractions.size())
+                .collect(Collectors.toList());
+
+        nearbyAttractions.addAll(additionalAttractions);
+    }
+
+    System.out.println("Nombre d'attractions à proximité : " + nearbyAttractions.size());
+
+    return nearbyAttractions;
+}
+
+
+*/
 
 	private void addShutDownHook() {
 		Runtime.getRuntime().addShutdownHook(new Thread() {
@@ -160,5 +296,4 @@ public class TourGuideService {
 		LocalDateTime localDateTime = LocalDateTime.now().minusDays(new Random().nextInt(30));
 		return Date.from(localDateTime.toInstant(ZoneOffset.UTC));
 	}
-
 }
