5a416ded2ef3abdd822f76548541d58c14a74bcb
Benmammar <bader.benmaamar@gmail.com>
2023-12-13 17:00:13 +0000

    Réduire THREAD_POOL_SIZE à 30

--- src/main/java/com/openclassrooms/tourguide/service/RewardsService.java
+++ src/main/java/com/openclassrooms/tourguide/service/RewardsService.java
@@ -27,7 +27,7 @@ public class RewardsService {
 	private final GpsUtil gpsUtil;
 	private final RewardCentral rewardsCentral;
 	
-	private static final int THREAD_POOL_SIZE = 40;
+	private static final int THREAD_POOL_SIZE = 30;
 
 	
 	public static int getThreadPoolSize() {
