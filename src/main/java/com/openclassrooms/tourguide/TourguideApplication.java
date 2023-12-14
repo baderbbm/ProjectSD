70729b4aa06dd3f9ecb3448a83444e8ccf5c7a30
baderbbm <131297523+baderbbm@users.noreply.github.com>
2023-12-14 20:52:17 +0100

    Update TourguideApplication.java

--- src/main/java/com/openclassrooms/tourguide/TourguideApplication.java
+++ src/main/java/com/openclassrooms/tourguide/TourguideApplication.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.openclassrooms.tourguide;
 
 import org.springframework.boot.SpringApplication;
@@ -12,24 +11,3 @@ public class TourguideApplication {
 	}
 
 }
-
-
-
-=======
-package com.openclassrooms.tourguide;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class TourguideApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(TourguideApplication.class, args);
-	}
-
-}
-
-
-
->>>>>>> 7f1d4e9b2e4702a6b3a68813cb03ea30b141d2f6
